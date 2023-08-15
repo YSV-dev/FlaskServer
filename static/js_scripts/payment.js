15a052d573d7639dde0c082336553c0a386e936e
YSV <64962422+YSV-dev@users.noreply.github.com>
2023-08-15 12:04:36 +0500

    Delete static/js_scripts directory

--- static/js_scripts/payment.js
+++
@@ -1,7 +0,0 @@
-$('#copy').click(function() {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($('#text').text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-});
